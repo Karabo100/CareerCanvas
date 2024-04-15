d2d94bbd1b5705c0eec7dac0549f2cae1854a2f9
Karabo100 <127797943+Karabo100@users.noreply.github.com>
2024-04-14 23:33:00 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# execution instructions
+# 1. terminal prompt: uvicorn main:app --reload --port 8001
+# 2. go to http://localhost:8001/docs
+# 3. upload audio from user and execute
+# 4. should get response audio
+# 5. if web application doesn't work use postman to send request
+
+
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import StreamingResponse
 from dotenv import load_dotenv
@@ -24,6 +32,7 @@ async def post_audio(file: UploadFile):
     user_message = transcribe_audio(file)
     #print(user_message)
     chat_response = get_chat_response(user_message)
+    #print(chat_response)
     audio_output = text_to_speech(chat_response)
 
     def iterfile():
@@ -40,8 +49,6 @@ def transcribe_audio(file):
     #transcription = {"role": "user", "content": "Who won the world series in 2020?"}
     print(transcription)
     return transcription
-
-
 
 def get_chat_response(user_message):
     messages = load_messages()
@@ -62,8 +69,6 @@ def get_chat_response(user_message):
 
     return parsed_gpt_response
 
-
-
 def load_messages():
     messages = []
     file = 'database.json'
@@ -79,7 +84,6 @@ def load_messages():
         messages.append({"role": "system", "content": "You are interviewing the user for a system designer position. Ask short questions that are relevant to a entry level system designer. Your name is Ben. The user is Angela. Keep responses under 50 words."})
     return messages
 
-
 def save_messages(user_message, gpt_response):
     file = 'database.json'
     messages = load_messages()
@@ -88,9 +92,9 @@ def save_messages(user_message, gpt_response):
     with open(file, 'w') as f:
         json.dump(messages, f)
 
-
 def text_to_speech(text):
-    voice_id = 'bVMeCyTHy58xNoL34h3p'
+    #voice_id = 'bVMeCyTHy58xNoL34h3p'
+    voice_id = 'CYw3kZ02Hs0563khs1Fj'
     body = {
         "text": text,
         "model_id": "eleven_monolingual_v1",
@@ -111,13 +115,15 @@ def text_to_speech(text):
     url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}"
     try:
         response = requests.post(url, json=body, headers= headers)
+        #print(response)
+        #print("Response code:", response.status)
         if response.status_code == 200:
             return response.content
         else:
-            print("something went wrong")
+            print("text to speech went wrong")
     except Exception as e:
         print(e)
 
-# Send in audio, and have it transcribed
+#Send in audio, and have it transcribed
 
 #sent it to chatgpt and get response
